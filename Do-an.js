acf2452b7ce3b323ba4af5ea8b0909420bf7ac6d
Cuong Nguyen <cuong199670@gmail.com>
2022-02-27 17:02:44 +0700

    Cuong Code

---
+++ Do-an.js
@@ -0,0 +1,213 @@
+$(document).ready(function() {
+    $("#all").click(function(){
+        $("#demo1_A").toggle();
+        $("#demo1_B").show();
+
+    });
+
+});
+
+
+
+/* Bài page FAQs */
+    /* cach 1 */
+    
+    /* $(document).ready(function() {
+        
+        $(".plusA1").click(function() {
+            $("#hutaA1").slideDown(function() {
+                $(".plusA1").hide();
+                $(".minusA1").show();
+            });
+            
+        });
+
+        
+        $(".minusA1").click(function() {
+            $("#hutaA1").slideUp(function()
+            {
+                $(".plusA1").show();
+                $(".minusA1").hide(); 
+            });
+            
+            
+        });
+    }); */
+    
+    /* cach 2 */
+    $(document).ready(function() {
+        /* slide down */
+        $(".plusA1").click(function() {
+            $("#hutaA1").slideDown(500); 
+                
+           
+            $(".plusA1").hide();
+            $(".minusA1").show();
+            
+        });
+
+        /* slide up */
+        $(".minusA1").click(function() {
+            $("#hutaA1").slideUp(500);
+            
+            $(".plusA1").show();
+            $(".minusA1").hide();
+            
+            
+        });
+
+        /* slide down A2 */
+        $(".plusA2").click(function() {
+            $("#hutaA2").slideDown(500); 
+                
+           
+            $(".plusA2").hide();
+            $(".minusA2").show();
+            
+        });
+
+        /* slide up  A2 */
+        $(".minusA2").click(function() {
+            $("#hutaA2").slideUp(500);
+            
+            $(".plusA2").show();
+            $(".minusA2").hide();
+            
+            
+        });
+
+
+        /* slide down A3 */
+        $(".plusA3").click(function() {
+            $("#hutaA3").slideDown(500); 
+                
+           
+            $(".plusA3").hide();
+            $(".minusA3").show();
+            
+        });
+
+        /* slide up  A3 */
+        $(".minusA3").click(function() {
+            $("#hutaA3").slideUp(500);
+            
+            $(".plusA3").show();
+            $(".minusA3").hide();
+            
+            
+        });
+
+        /* slide down A4 */
+        $(".plusA4").click(function() {
+            $("#hutaA4").slideDown(500); 
+                
+           
+            $(".plusA4").hide();
+            $(".minusA4").show();
+            
+        });
+
+        /* slide up  A4 */
+        $(".minusA4").click(function() {
+            $("#hutaA4").slideUp(500);
+            
+            $(".plusA4").show();
+            $(".minusA4").hide();
+            
+            
+        });
+
+
+        /* slide down B1 */
+        $(".plusB1").click(function() {
+            $("#hutaB1").slideDown(500); 
+                
+           
+            $(".plusB1").hide();
+            $(".minusB1").show();
+            
+        });
+
+        /* slide up  B1 */
+        $(".minusB1").click(function() {
+            $("#hutaB1").slideUp(500);
+            
+            $(".plusB1").show();
+            $(".minusB1").hide();
+            
+            
+        });
+
+
+        /* slide down B2 */
+        $(".plusB2").click(function() {
+            $("#hutaB2").slideDown(500); 
+                
+           
+            $(".plusB2").hide();
+            $(".minusB2").show();
+            
+        });
+
+        /* slide up  B2 */
+        $(".minusB2").click(function() {
+            $("#hutaB2").slideUp(500);
+            
+            $(".plusB2").show();
+            $(".minusB2").hide();
+            
+            
+        });
+
+
+
+        /* slide down B3 */
+        $(".plusB3").click(function() {
+            $("#hutaB3").slideDown(500); 
+                
+           
+            $(".plusB3").hide();
+            $(".minusB3").show();
+            
+        });
+
+        /* slide up  B3 */
+        $(".minusB3").click(function() {
+            $("#hutaB3").slideUp(500);
+            
+            $(".plusB3").show();
+            $(".minusB3").hide();
+            
+            
+        });
+
+        /* slide down B4 */
+        $(".plusB4").click(function() {
+            $("#hutaB4").slideDown(500); 
+                
+           
+            $(".plusB4").hide();
+            $(".minusB4").show();
+            
+        });
+
+        /* slide up  B4 */
+        $(".minusB4").click(function() {
+            $("#hutaB4").slideUp(500);
+            
+            $(".plusB4").show();
+            $(".minusB4").hide();
+            
+            
+        });
+
+
+
+
+    
+    });
+
+  
+
+
+
